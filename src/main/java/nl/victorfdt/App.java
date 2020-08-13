07243b8fe08e1ce3b02aa5bf0ac4b207b2b247b6
Victor Di Trani <victor.ditrani@isaac.nl>
2020-08-13 16:20:18 +0200

    Add Nested Class example

--- src/main/java/nl/victorfdt/App.java
+++ src/main/java/nl/victorfdt/App.java
@@ -1,11 +1,16 @@
 package nl.victorfdt;
 
 import nl.victorfdt.enums.LearnEnums;
+import nl.victorfdt.nestedclasses.Building;
 import nl.victorfdt.utils.Exercise;
 
 public class App {
     public static void main(String[] args) {
-        Exercise exercise = new LearnEnums();
+        //Learn Enums
+        //Exercise exercise = new LearnEnums();
+
+        // Learn Nested Classes
+        Exercise exercise = new Building();
         exercise.execute();
     }
 }
---
+++ src/main/java/nl/victorfdt/nestedclasses/Building.java
@@ -0,0 +1,70 @@
+package nl.victorfdt.nestedclasses;
+
+import nl.victorfdt.utils.Exercise;
+
+public class Building implements Exercise {
+    private String name;
+    private static String address;
+
+    public Building() {
+        System.out.println("Building constructor");
+        this.name = "Company inc.";
+        this.address = "From Building: Somewhere 123";
+    }
+
+    private void callingMethods() {
+        //Instantiate the Inner class and use its methods.
+        BuildingHelper helper = this.new BuildingHelper();
+
+        //It is not print the information from the constructor because it was not initialized
+        helper.showInformation();
+        this.new BuildingHelper().showInformation();
+
+        //It will still not display it, because the helper was created using the older Building
+        //class.
+        Building b = new Building();
+        System.out.println(b.name);
+        helper.showInformation();
+
+        Building bb = new Building();
+        helper = bb.new BuildingHelper();
+        helper.showInformation();
+    }
+
+    private void callingVariables() {
+        // Inner classes can have variables with same name as the Outer class.
+        // There is a special way to use this and access each variable.
+        BuildingHelper helper = this.new BuildingHelper();
+
+        //Access information from the Outer Class
+        System.out.println(String.format("Building name: {}, address: {}", this.name, this.address));
+
+        //Access information from the Inner Class
+        System.out.println(String.format("BuildingHelper janitorName: {}, address: {}",
+                this.new BuildingHelper().janitorName, helper.address));
+    }
+
+    public void execute() {
+        callingMethods();
+        callingVariables();
+    }
+
+    public class BuildingHelper {
+        private String janitorName;
+        private String address;
+
+        public BuildingHelper() {
+            System.out.println("BuildingHelper constructor");
+            this.janitorName = "Jorge";
+            this.address = "From BuildingHelper: Somewhere 123";
+        }
+
+        public void cleanRooms() {
+            System.out.println("The rooms were cleaned.");
+        }
+
+        public void showInformation() {
+            System.out.println(String.format("Name: {}, Address: {}", Building.this.name, Building.this.address));
+        }
+    }
+}
